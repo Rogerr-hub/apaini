67849d6ae14bda5ffcada8a5e4f62b8b3e97a5e8
Rogerr-hub <rogerr010200@gmail.com>
2024-11-05 21:45:43 +0700

    Create Object.c

---
+++ Object.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+/**
+ * Struct representing a generic object in C.
+ * This is similar to Java's Object class, acting as the root of all structures.
+ */
+typedef struct Object {
+    // Fields can be added here to represent data
+} Object;
+
+/**
+ * Constructs an Object.
+ * In C, we can just return an empty Object struct.
+ */
+Object createObject() {
+    Object obj;
+    // Initialize any fields if needed
+    return obj;
+}
+
+/**
+ * Compares two Object references for equality.
+ * In C, we compare pointers to simulate Java's 'equals' method.
+ *
+ * @param obj1 The first object to compare.
+ * @param obj2 The second object to compare.
+ * @return true if the objects are the same; false otherwise.
+ */
+bool equals(Object *obj1, Object *obj2) {
+    return (obj1 == obj2);  // Compare object pointers (similar to Java's `==` operator)
+}
+/*
+int main() {
+    Object obj1 = createObject();  // Create first object
+    Object obj2 = createObject();  // Create second object
+
+    // Compare obj1 and obj2
+    if (equals(&obj1, &obj2)) {
+        printf("Objects are equal.\n");
+    } else {
+        printf("Objects are not equal.\n");
+    }
+
+    // Compare obj1 with itself
+    if (equals(&obj1, &obj1)) {
+        printf("Object is equal to itself.\n");
+    }
+
+    return 0;
+}
+*/
