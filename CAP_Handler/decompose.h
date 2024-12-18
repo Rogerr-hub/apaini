4631e2cdde0188a0349350bde49ef97ca2134eb6
hubsch-jaeger <hubsch.jaeger@gmail.com>
2024-12-18 11:23:28 +0700

    Update decompose.h

--- CAP_Handler/decompose.h
+++ CAP_Handler/decompose.h
@@ -9,3 +9,7 @@
 void header_structure(uint8_t *buffer, size_t length, header *hdr);
 void applet_structure_versi_2_1(uint8_t *buffer, size_t length, applet *aplt);
 void directory_structure_versi_2_1(uint8_t *buffer, size_t length, directory *drt);
+void import_structure_versi_2_1(uint8_t *buffer, size_t length, import *imp);
+void cp_structure_versi_2_1(uint8_t* buffer, size_t length, constantpool* cp);
+void sf_structure_versi_2_1(uint8_t* buffer, size_t length, static_field* sf);
+void reflo_structure_versi_2_1(uint8_t* buffer, size_t length, RefLocation* rf);
